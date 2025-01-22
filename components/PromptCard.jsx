1c70e7a5a60c412a197aa1c9975a0b97b630f149
Rohit Gami <rohitgami2003@gmail.com>
2025-01-22 22:16:54 +0530

    update PromptCard CSS and make link to conversation clickable

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -11,7 +11,6 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const router = useRouter();
 
   const [copied, setCopied] = useState("");
-  const [copiedLink, setCopiedLink] = useState("");
 
   const handleProfileClick = () => {
     console.log(post);
@@ -28,31 +27,31 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   };
 
   return (
-    <div className='prompt_card'>
-      <div className='flex justify-between items-start gap-5'>
+    <div className="prompt_card max-w-md mx-auto p-4 bg-white shadow-md rounded-lg">
+      <div className="flex justify-between items-start gap-5">
         <div
-          className='flex-1 flex justify-start items-center gap-3 cursor-pointer'
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
           onClick={handleProfileClick}
         >
           <Image
             src={post.creator.image}
-            alt='user_image'
+            alt="user_image"
             width={40}
             height={40}
-            className='rounded-full object-contain'
+            className="rounded-full object-contain"
           />
 
-          <div className='flex flex-col'>
-            <h3 className='font-satoshi font-semibold text-gray-900'>
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
               {post.creator.username}
             </h3>
-            <p className='font-inter text-sm text-gray-500'>
+            <p className="font-inter text-sm text-gray-500">
               {post.creator.email}
             </p>
           </div>
         </div>
 
-        <div className='copy_btn' onClick={handleCopy}>
+        <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
               copied === post.prompt
@@ -66,59 +65,48 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
         </div>
       </div>
 
-      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
-      <p
-        className="font-inter text-sm"
-      >
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p className="font-inter text-sm">
         {post.link && (
-          <div className="flex items-center gap-2 mb-4"> 
-            
+          <div className="flex items-center gap-2 mb-4">
             <img
-              src={
-                copiedLink === post.Link
-                  ? "/assets/icons/tick.svg"
-                  : "/assets/images/iconmonstr-link-thin.svg"
-              }
+              src={"/assets/images/iconmonstr-external-link-thin.svg"}
               alt="Link"
               onClick={() => {
-                setCopiedLink(post.Link);
-                navigator.clipboard.writeText(post.link);
-                setTimeout(() => setCopiedLink(false), 2000);
+                window.open(post.link, "_blank", "noopener,noreferrer");
               }}
-              className="cursor-pointer w-5 h-5" 
+              className="cursor-pointer w-4 h-4"
             />
-            
+
             <span className="text-sm font-medium text-black">
               Link to a conversation
             </span>
           </div>
         )}
       </p>
-      <p
-        className='font-inter text-sm blue_gradient cursor-pointer'
-        // onClick={() => handleTagClick && handleTagClick(post.tag)}
-      >
-        {post.tags && post.tags.map((tag, index) => (
-          <span
-            key={index}
-            onClick={() => handleTagClick && handleTagClick(tag)}
-            className="mr-2"
-          >
-            #{tag}
-          </span>
-        ))}
+      <p className="font-inter text-sm blue_gradient cursor-pointer">
+        {post.tags &&
+          post.tags.map((tag, index) => (
+            <span
+              key={index}
+              onClick={() => handleTagClick && handleTagClick(tag)}
+              className="mr-2"
+            >
+              #{tag}
+            </span>
+          ))}
       </p>
 
       {session?.user.id === post.creator._id && pathName === "/profile" && (
-        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p
-            className='font-inter text-sm green_gradient cursor-pointer'
+            className="font-inter text-sm green_gradient cursor-pointer"
             onClick={handleEdit}
           >
             Edit
           </p>
           <p
-            className='font-inter text-sm orange_gradient cursor-pointer'
+            className="font-inter text-sm orange_gradient cursor-pointer"
             onClick={handleDelete}
           >
             Delete
